89adca8a8ffce9c1c0fc83de6e396fc95c0dd509
Lafleur, Justin <jlafleur@ciena.com>
2016-01-27 16:14:29 -0500

    fix console errors

--- addon/helpers/fr-markdown-file.js
+++ addon/helpers/fr-markdown-file.js
@@ -1,20 +1,23 @@
 import Ember from 'ember';
 import markdownFiles from 'ember-fr-markdown-file-strip-number-prefix/markdownFiles';
 
-Object.prototype.renameProperty = function (oldName, newName) {
+function renameProperty(obj, oldName, newName) {
      // Do nothing if the names are the same
-     if (oldName == newName) {
-         return this;
+     if (oldName === newName) {
+         return obj;
      }
     // Check for the old property name to avoid a ReferenceError in strict mode.
-    if (this.hasOwnProperty(oldName)) {
-        this[newName] = this[oldName];
-        delete this[oldName];
+    if (obj.hasOwnProperty(oldName)) {
+        obj[newName] = obj[oldName];
+        delete obj[oldName];
     }
-    return this;
+    return obj;
 };
+
 function clone(obj) {
-    if (null == obj || "object" != typeof obj) return obj;
+    if (null === obj || "object" !== typeof obj){
+      return obj;
+    }
     var copy = obj.constructor();
     for (var attr in obj) {
         if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
@@ -31,20 +34,20 @@ function dive(obj){
     if (!obj.hasOwnProperty(key)) continue;
     var prop = obj[key];
 
-    if (typeof prop == 'object'){
+    if (typeof prop === 'object'){
       // console.log(prop + " is an object");
       var newProp = dive(clone(prop));
       newObject[key] = newProp;
     }
-    newObject.renameProperty(arr[i], arr[i].toString().substring(3))
+    renameProperty(newObject, arr[i], arr[i].toString().substring(3))
     i++;
   }
   return newObject;
 }
 export function frMarkdownFile([path]) {
-console.log(markdownFiles);
+  console.log(markdownFiles);
   var strippedMarkdownFiles = dive(clone(markdownFiles));
-    console.log(strippedMarkdownFiles);
+  console.log(strippedMarkdownFiles);
   let markdown = Ember.get(strippedMarkdownFiles, path.replace(/\//g, '.'));
   return markdown;
 }
